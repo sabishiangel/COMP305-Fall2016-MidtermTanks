888d24a2ce440f2ffd4cbef15cfa52c684f182f1
sabishiangel <ashley.tjonhing@gmail.com>
2016-10-22 09:14:43 -0400

    initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankCount;
+	public GameObject tank;
+	
+	// Use this for initialization
+	void Start () {
+		this._GenerateTanks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate Clouds
+	private void _GenerateTanks() {
+		for (int count=0; count < this.tankCount; count++) {
+			Instantiate(tank);
+		}
+	}
+}
